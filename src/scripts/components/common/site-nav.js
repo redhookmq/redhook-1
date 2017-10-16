928c59b71c44c0b3ca603d1727b24e2ebe6418bd
Steven Waller <jstevenwaller@gmail.com>
2017-10-16 13:39:14 -0700

    Desktop sticky nav functionality

--- src/scripts/components/common/site-nav.js
+++ src/scripts/components/common/site-nav.js
@@ -1,20 +1,42 @@
 import $ from 'jquery';
+import debounce from 'lodash/debounce';
 
+const $window = $(window);
 const $body = $('body');
 const $mask = $('.site-container__mask');
+const $siteNav = $('.site-nav');
 const $menuBtn = $('.menu-btn');
+let windowHeight = $window.height();
+let isSticky = false;
+
+// init
+// ----------------------------------------- //
+const init = function () {
+  checkScrollPosition();
+  bind();
+};
 
 // bind
 // ----------------------------------------- //
-$menuBtn.click((e) => {
-  e.preventDefault();
-  openNav();
-});
+const bind = function () {
+  $menuBtn.click((e) => {
+    e.preventDefault();
+    openNav();
+  });
 
-$mask.click((e) => {
-  e.preventDefault();
-  closeNav();
-});
+  $mask.click((e) => {
+    e.preventDefault();
+    closeNav();
+  });
+
+  $window.on('scroll', () => {
+    checkScrollPosition();
+  });
+
+  $window.on('resize', debounce(() => {
+    windowHeight = $window.height();
+  }, 300));
+};
 
 // methods
 // ----------------------------------------- //
@@ -25,3 +47,17 @@ const openNav = function () {
 const closeNav = function () {
   $body.removeClass('has-open-nav');
 };
+
+const checkScrollPosition = function () {
+  if ($window.scrollTop() > windowHeight - 84) {
+    if (isSticky === false) {
+      isSticky = true;
+      $siteNav.addClass('is-sticky');
+    }
+  } else if (isSticky) {
+    isSticky = false;
+    $siteNav.removeClass('is-sticky');
+  }
+};
+
+init();
